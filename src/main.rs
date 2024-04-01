b01c2b4dbff7caca6272038b4aee5dcce31ed018
ZhenQian <zhenqian@keyst.one>
2024-04-01 14:59:50 +0800

    test cargo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// define a person struct
+struct Person {
+    name: String,
+    age: u8,
+}
+
 fn main() {
+    // create a person
+    let person = Person {
+        name: String::from("Alice"),
+        age: 30,
+    };
+
+    println!("Name: {}", person.name);
+
     println!("Hello, world!");
 }
